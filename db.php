00399cd64dcc5dbcb78aef9eac2ce058cb2d8719
Vatsal Kamlesh Patel <55306198+VatsalPatel19@users.noreply.github.com>
2024-07-16 21:47:25 -0400

    Add files via upload

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "farm_database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
